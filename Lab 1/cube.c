9f23db4b8c8cb367353a5c000d591979023ac412
Martin <marki423@student.liu.se>
2015-04-20 14:57:26 +0200

    Fixed lighting and other stuff

--- Lab 1/cube.c
+++ Lab 1/cube.c
@@ -2,15 +2,19 @@
 
 #include <GL/glut.h>
 
-GLdouble angle;
+GLdouble angle, angle2;
 GLfloat sun_color[] = { 0.9, 0.0, 0.0, 1.0 };
 GLfloat sun_spec[] = { 0.5, 0.5, 0.5, 1.0 };
 const GLfloat center[] = { 0, 0, -3, 1 };
-GLfloat ambient0[] = { 0, 0, 0 ,1};
+GLfloat ambient0[] = { -0.1, -0.1, -0.1 ,1};
 GLfloat diffuse0[] = { 0.5, 0.5, 0.5, 1 };
 GLfloat specular0[] = { 0.9, 0.5, 0, 1 };
 GLfloat sun_emission[] = { 0.9, 0.3, 0.1, 0.1 };
+GLfloat earth_color[] = { 0.1, 0.6, 0.3, 1 };
 
+//earthTexture = LoadBitmap("earth_texture.jpg");
+GLuint earthTexture;
+//Image* image = loadBMP("earth_texture.bmp");
 void init() {
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
@@ -19,6 +23,7 @@ void init() {
 
     glClearColor(0, 0, 0, 0);
     angle = 0;
+	angle2 = 0;
 	
 	glLightfv(GL_LIGHT0, GL_POSITION, center);
 	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient0);
@@ -28,8 +33,10 @@ void init() {
 }
 
 void anim() {
-    angle += 0.5;
+    angle += 0.08;
+	angle2 += 2.4;
     if (angle>360) angle -= 360;
+	if (angle2>360) angle -= 360;
     glutPostRedisplay();
 }
 
@@ -41,29 +48,41 @@ void display(void) {
 	
 	
     glTranslatef(0, 0, -3);
+	glEnable(GL_COLOR_MATERIAL);
 
 	glPushMatrix();
 	glRotatef(angle, 0, 1, 0);
     glShadeModel(GL_FLAT);
-	glMaterialfv(GL_FRONT, GL_AMBIENT, ambient0);
+	//glMaterialfv(GL_FRONT, GL_AMBIENT, ambient0);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, sun_color);
     glMaterialfv(GL_FRONT, GL_SPECULAR, sun_spec);
 	glMaterialfv(GL_FRONT, GL_EMISSION, sun_emission);
     glutSolidSphere(0.2, 100, 100);
 	glPopMatrix();
-
+	glDisable(GL_COLOR_MATERIAL);
 	
+	glEnable(GL_COLOR_MATERIAL);
 	glPushMatrix();
 	glRotatef(angle, 0, 1, 0);
 	glTranslatef(1, 0, 0);
-	glRotatef(angle, 0, 1, 0);
+	glRotatef(angle2, 0, 1, 0);
+	glRotatef(10, 0, 1, 0);
 	glMaterialfv(GL_FRONT, GL_AMBIENT, ambient0);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, sun_color);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, sun_spec);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, earth_color);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, earth_color);
 	glMaterialfv(GL_FRONT, GL_EMISSION, ambient0);
-
-	glutSolidSphere(0.05, 100, 100);
-
+	glEnable(GL_TEXTURE_2D);
+	//glMatrixMode(GL_TEXTURE);
+	glDisable(GL_LIGHT0);
+	glBegin(GL_LINES);
+	glVertex3f(0.0, 0.15, 0.0);
+	glVertex3f(0.0, -0.15, 0.0);
+	glEnd();
+	glEnable(GL_LIGHT0);
+	//LoadTexture("earth_texture.bmp", earthTexture);
+	//glBindTexture(GL_TEXTURE_2D, earthTexture);
+	glutSolidSphere(0.05, 20, 20);
+	//glDisable(GL_TEXTURE_2D);
 	glPopMatrix();
 	//glPopMatrix();
 
